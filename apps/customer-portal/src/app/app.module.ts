385dc404e7dc0a6a6637c8a81c5a2026d29e50e7
Sandi <sandis@ebe-inc.com>
2020-11-06 10:24:41 +0100

    created the auth library, and scaffolded the login component

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { authRoutes, AuthModule } from '@demo-app/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([{ path: 'auth', children: authRoutes }], {
+      initialNavigation: 'enabled',
+    }),
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
