decb6622e67665e1920be4159102c3016742ffff
Sandi <sandis@ebe-inc.com>
2020-11-06 11:51:40 +0100

    installed some missing packages

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './containers/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 
@@ -9,7 +10,7 @@ export const authRoutes: Route[] = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   declarations: [LoginComponent, LoginFormComponent],
 })
 export class AuthModule {}
