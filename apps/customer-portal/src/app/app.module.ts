db4538b2cbdf4cdbd38503d987235483b3b6664b
Sandi <sandis@ebe-inc.com>
2020-11-09 23:58:11 +0100

    added store module to customer-portal

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { RouterModule } from '@angular/router';
 import { authRoutes, AuthModule, AuthGuard } from '@demo-app/auth';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@demo-app/layout';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,6 +36,19 @@ import { LayoutModule } from '@demo-app/layout';
     ),
     LayoutModule,
     AuthModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './containers/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { MaterialModule } from '@demo-app/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './interceptors/auth/auth.interceptor';
 
 export const authRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
 ];
+
+const COMPONENTS = [LoginComponent, LoginFormComponent];
 
 @NgModule({
   imports: [
@@ -20,6 +23,14 @@ export const authRoutes: Route[] = [
     ReactiveFormsModule,
     FormsModule,
   ],
-  declarations: [LoginComponent, LoginFormComponent],
+  declarations: [...COMPONENTS],
+  exports: [...COMPONENTS],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class AuthModule {}
