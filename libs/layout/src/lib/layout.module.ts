b6fdfb246debdafec7bcd82ce6e9605adcc1a32d
Sandi <sandis@ebe-inc.com>
2020-11-09 21:41:31 +0100

    updated the layout.component

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './containers/layout/layout.component';
+import { MaterialModule } from '@demo-app/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   declarations: [LayoutComponent],
+  exports: [LayoutComponent],
 })
 export class LayoutModule {}
